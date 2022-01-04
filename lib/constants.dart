5f8ca47d830e14a60d9adf00fb9fcabd1ee17b77
AlexAntoine <anotoinealex1234@gmail.com>
2022-01-04 03:11:47 -0500

    added style page

---
+++ lib/constants.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kCardColors = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerCardColor = Color(0xFFEB1555);
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kLargeButtonText = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultsTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextstyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
